2a8ae21d7029682a4af363947d1739ae100cfb11
Khawakii <podachloe5@gmail.com>
2026-01-21 01:17:56 +0100

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/league_web/',
   plugins: [
     vue(),
     vueDevTools(),
